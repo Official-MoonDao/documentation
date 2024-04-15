7578cb883f486873acc144416ff0f3bb2d938a3b
colinmfoster4723 <colin.foster4723@gmail.com>
2024-04-15 10:13:48 -0500

    Adjusted layout

--- .site-config/quartz.layout.ts
+++ .site-config/quartz.layout.ts
@@ -20,22 +20,21 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    Component.TableOfContents(),
   ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(
-      Component.Explorer({
-        filterFn: (node) => {
-          // set containing names of folders to filter out from sidebar
-          const omit = new Set(["reference"]);
-          return !omit.has(node.name.toLowerCase());
-        },
-      })
-    ),
+
+    Component.Explorer({
+      filterFn: (node) => {
+        // set containing names of folders to filter out from sidebar
+        const omit = new Set(["reference"]);
+        return !omit.has(node.name.toLowerCase());
+      },
+    }),
   ],
   right: [Component.Graph(), Component.Backlinks()],
 };
